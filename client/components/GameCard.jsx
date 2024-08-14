8d48912ca6173b83fbf83104c29d83db5cf21c17
KariRomero <karinaromero.1993@gmail.com>
2024-08-14 16:12:15 -0300

    GameCard: Tailwind styles done

--- client/components/GameCard.jsx
+++ client/components/GameCard.jsx
@@ -1,58 +1,35 @@
 import { useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, Image, Animated } from 'react-native';
+import { Text, View, Image, Animated } from 'react-native';
 
 export function GameCard({ game }) {
     return (
-        <View key={game.slug} style={styles.card}>
-            <Image source={{ uri: game.image }} style={styles.image} />
-            <Text style={styles.title}>{game.title}</Text>
-            <Text style={styles.description}>{game.description}</Text>
-            <Text style={styles.score}>{game.score}</Text>
+        <View
+            className="flex-row bg-gray-500/10 p-4 rounded-xl gap-4 mb-10"
+            key={game.slug}>
+            <Image source={{ uri: game.image }} className='w-32 h-60' />
+            <View>
+                <Text className='text-lg text-white'>{game.title}</Text>
+                <Text className='text-white text-xs'>{game.description}</Text>
+                <Text className='text-green-600 text-lg'>{game.score}</Text>
+            </View>
         </View>
     )
 };
 
-export function AnimatedGameCard({ game, index }){
+export function AnimatedGameCard({ game, index }) {
     const opacity = useRef(new Animated.Value(0)).current
 
-    useEffect(()=>{
-        Animated.timing(opacity,{
-            toValue:1,
-            duration:500,
+    useEffect(() => {
+        Animated.timing(opacity, {
+            toValue: 1,
+            duration: 500,
             delay: index * 500,
-            useNativeDriver:true
+            useNativeDriver: true
         }).start()
-    },[opacity,index])
-    return(
+    }, [opacity, index])
+    return (
         <Animated.View style={{ opacity }}>
-            <GameCard game={game}/>
+            <GameCard game={game} />
         </Animated.View>
     )
 }
-
-const styles = StyleSheet.create({  
-    card: {
-      marginBottom: 10,
-    },
-    image: {
-      width: 107,
-      height: 147,
-      borderRadius: 10,
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      marginBottom: 10,
-      color: '#fff',
-    },
-    description: {
-      fontSize: 16,
-      color: '#eee',
-    },
-    score: {
-      fontSize: 16,
-      fontWeight: 'bold',
-      color: 'green',
-      marginTop: 10,
-    },
-  });
